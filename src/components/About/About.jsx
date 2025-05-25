384e550022ab31927a07028ab8385b9523b66851
Dushyant Sharma <148749504+dushyantsharma460@users.noreply.github.com>
2025-05-25 22:50:28 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:w-5/12 lg:w-5/12">
                         <img
-                            src="public/About.png"
+                            src="/About.png"
                             alt="Panda and Vulture"
                             className="rounded-lg shadow-lg"
                         />
